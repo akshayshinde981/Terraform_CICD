bc8798824cf0dee9f820f8ac25267baa3cbdd9ac
akshayshinde981 <akshayshinde981@gmail.com>
2024-09-30 22:29:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "eu-north-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-00db8dadb36c9815e"
+    ami = "ami-0129bfde49ddb0ed6"
     instance_type = "t2.nano"
     tags = {
       Name = "dev-ec3"
